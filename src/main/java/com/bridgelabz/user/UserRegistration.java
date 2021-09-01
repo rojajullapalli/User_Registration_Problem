78cc42cbc3f4948105c7e84c1c38a030f7b608db
rojajullapalli <rojajules@gmail.com>
2021-09-01 20:47:43 +0530

    ADDED: Sample Email

--- src/main/java/com/bridgelabz/user/UserRegistration.java
+++ src/main/java/com/bridgelabz/user/UserRegistration.java
@@ -12,7 +12,8 @@ public class UserRegistration {
     }
 
     public static boolean validEmail(String email){
-        String EmailRegex = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+        //String EmailRegex = "^[a-zA-Z0-9.!#$&'+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+        String EmailRegex = "[0-9a-zA-Z]+([0-9a-zA-Z]*[-._+])*[0-9a-zA-Z]+@[0-9a-zA-Z]+([-.][0-9a-zA-Z]+)*([0-9a-zA-Z]*[.])[a-zA-Z]{2,6}";
         return email.matches(EmailRegex);
     }
 
--- src/test/java/com/bridgelabz/user/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/user/UserRegistrationTest.java
@@ -35,14 +35,54 @@ public class UserRegistrationTest {
     public void givenEmail_WhenProper_ShouldReturnTrue() {
         UserRegistration registration = new UserRegistration();
         boolean result = registration.validEmail("abc.xyz@bl.co.in");
+        boolean result1 = registration.validEmail("abc@yahoo.com");
+        boolean result2 = registration.validEmail("abc-100@yahoo.com");
+        boolean result3 = registration.validEmail("abc.100@yahoo.com");
+        boolean result4 = registration.validEmail("abc-100@abc.net");
+        boolean result5 = registration.validEmail("abc.100@abc.com.au");
+        boolean result6 = registration.validEmail("abc@1.com");
+        boolean result7 = registration.validEmail("abc@gmail.com.com");
+        boolean result8 = registration.validEmail("abc+100@gmail.com");
+        boolean result9 = registration.validEmail("abc111@abc.net");
         Assertions.assertTrue(result);
+        Assertions.assertTrue(result1);
+        Assertions.assertTrue(result2);
+        Assertions.assertTrue(result3);
+        Assertions.assertTrue(result4);
+        Assertions.assertTrue(result5);
+        Assertions.assertTrue(result6);
+        Assertions.assertTrue(result7);
+        Assertions.assertTrue(result8);
+        Assertions.assertTrue(result9);
     }
 
     @Test
     public void givenEmail_WhenImProper_ShouldReturnFalse() {
         UserRegistration registration = new UserRegistration();
         boolean result = registration.validEmail("abc.in");
+        boolean result1 = registration.validEmail("abc");
+        boolean result2 = registration.validEmail("abc@.com.my");
+        boolean result3 = registration.validEmail("abc123@gmail.a");
+        boolean result4 = registration.validEmail("abc123@.com");
+        boolean result5 = registration.validEmail("abc123@.com.com");
+        boolean result6 = registration.validEmail(".abc@abc.com");
+        boolean result7 = registration.validEmail("abc()*@gmail.com");
+        boolean result8 = registration.validEmail("abc@%*.com");
+        boolean result10 = registration.validEmail("abc.@gmail.com");
+        boolean result11 = registration.validEmail("abc@abc@gmail.com");
+        boolean result12 = registration.validEmail("abc@gmail.com.1a");
         Assertions.assertFalse(result);
+        Assertions.assertFalse(result1);
+        Assertions.assertFalse(result2);
+        Assertions.assertFalse(result3);
+        Assertions.assertFalse(result4);
+        Assertions.assertFalse(result5);
+        Assertions.assertFalse(result6);
+        Assertions.assertFalse(result7);
+        Assertions.assertFalse(result8);
+        Assertions.assertFalse(result10);
+        Assertions.assertFalse(result11);
+        Assertions.assertFalse(result12);
     }
 
     @Test
